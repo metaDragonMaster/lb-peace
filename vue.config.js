899319b0aff6804a81746cb239ec2cb4523acb49
metaDragonMaster <2327139286@qq.com>
2022-05-09 11:40:37 +0800

    change dist path

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = {
 	lintOnSave: true,
 	publicPath: './', // 基本路径
-	outputDir: 'nft-peace-dist', // 输出文件目录
+	outputDir: 'v1-lb-peace-dist', // 输出文件目录
 	productionSourceMap: false, // 生产环境是否生成 sourceMap 文件
 	css: {
 		requireModuleExtension: true,
